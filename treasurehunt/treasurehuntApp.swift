132ea29f29d97dcb766edaa0bd6c7ad6ca83e8c4
Isaque <isaquetargino144@gmail.com>
2026-07-01 14:38:59 -0400

    Initial Commit

---
+++ treasurehunt/treasurehuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  treasurehuntApp.swift
+//  treasurehunt
+//
+//  Created by Isaque da Silva Targino on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct treasurehuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
